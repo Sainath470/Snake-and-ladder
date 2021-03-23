7ef19d17fc74e9e884e69f6d723deb9b70bbc721
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-24 01:14:49 +0530

    Modified: till the player reahces 100 position

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -7,30 +7,35 @@ public class SnakeandLadder
 		int STARTPOSITION=0;
 		int Player1=STARTPOSITION;
 
-		Random roll = new Random();
-		int rollDice= roll.nextInt(6)+1;
+		   while(Player1<=100)
+		  	{
+				Random roll = new Random();
+				int rollDice= roll.nextInt(6)+1;
 
-		System.out.println(rollDice);
+				Random opt =  new Random();
+				int Options= opt.nextInt(3);
 
-		Random opt =  new Random();
-		int Options= opt.nextInt(3);
+						switch(Options)
+						{
+								case 0: 	Player1=Player1;
+											break;
 
-			switch(Options)
-			{
-					case 0: 	Player1=Player1;
-								System.out.println("You remain at same position");
-								break;
+								case 1:  Player1=Player1+rollDice;
+											break;
 
-					case 1:  Player1=Player1+rollDice;
-								System.out.println("Congrats! you got the ladder so moving "+Player1+" steps forward");
-								break;
+								case 2:	Player1=Player1-rollDice;
+											break;
 
-					case 2:	Player1=Player1-rollDice;
-								System.out.println("Oops! you got bitten by snake so moving "+Player1+" steps backward");
-								break;
+				  				default:  System.out.println("Invalid");
+											 break;
+						}
 
-				  default:  System.out.println("Invalid");
-								break;
+								if(Player1<0)
+								{
+										Player1=STARTPOSITION;
+								}
+
 			}
+			System.out.println("Congratulations you have reached 100 and won the game");
 	}
 }
