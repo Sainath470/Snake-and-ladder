428b5c32ac51915094a249cea3b77bbd99fe20a3
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-23 21:26:41 +0530

    Added:Single player starting position

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,1 +1,11 @@
-"Welcome to the snake and ladder game"
+//"Welcome to the snake and ladder game"
+public class SnakeandLadder
+{
+	pubic static void main(String args[])
+	{
+		int STARTPOSITION=0;
+		int player1=STARTPOINT;
+
+	}
+
+}
