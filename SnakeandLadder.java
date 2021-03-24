494f12fdd873387cff1a54916545c7d90224ec48
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-24 20:18:21 +0530

    Added:Second player and in case of ladder player rolls dice again

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -6,26 +6,42 @@ public class SnakeandLadder
 	{
 		int STARTPOSITION=0;
 		int Player1=STARTPOSITION;
-		int rollDiceCount=0;
+		int Player2=STARTPOSITION;
+		int rollDiceCount1=0;
+		int rollDiceCount2=0;
 
-		   while(Player1<=100)
+		   while((Player1<=100)&&(Player2<=100))
 		  	{
-				Random roll = new Random();
-				int rollDice= roll.nextInt(6)+1;
-				rollDiceCount++;
+					//rolling for player 1
+				Random roll1 = new Random();
+				int rollDice1= roll1.nextInt(6)+1;
+				rollDiceCount1++;
+					
+					//rolling for player2
+					 Random roll2 = new Random();
+            int rollDice2= roll2.nextInt(6)+1;
+            rollDiceCount2++;
 
-				Random opt =  new Random();
-				int Options= opt.nextInt(3);
+					//options for player 1
+				Random opt1 =  new Random();
+				int Options1= opt1.nextInt(3);
 
-						switch(Options)
+						//swtich for player1
+						switch(Options1)
 						{
 								case 0: 	Player1=Player1;
 											break;
+											
+												//Ladder
+								case 1:  Player1=Player1+rollDice1;
+											 Random rollAgain1=new Random();
+                                  int LadderRoll1=rollAgain1.nextInt(6)+1;
+												Player1=Player1+LadderRoll1;
+												rollDiceCount1++;
+												break;
 
-								case 1:  Player1=Player1+rollDice;
-											break;
-
-								case 2:	Player1=Player1-rollDice;
+											//Snake
+								case 2:	Player1=Player1-rollDice1;
 											break;
 
 				  				default:  System.out.println("Invalid");
@@ -39,18 +55,61 @@ public class SnakeandLadder
 
 								if(Player1>100)
 								{
-										Player1=Player1-rollDice;
+										Player1=Player1-rollDice1;
 								}
 								if(Player1==100)
 								{
 										break;
 								}
-								System.out.println("Position of Player is in "+Player1);
 
-			}
-			System.out.println("Number of times dice rolled "+rollDiceCount);
+								//options for player 2
+            				Random opt2 =  new Random();
+            				int Options2= opt2.nextInt(3);
 
-			System.out.println("Position of Player is "+Player1);
-			System.out.println("Congratulations Player has reached 100 position and won the game");
-	}
+								  //swtich for player1
+         			         switch(Options2)
+                  			{
+                        			case 0:  Player2=Player2;
+                            			     break;
+																	
+														//Ladder
+                        			case 1:  Player2=Player2+rollDice2;
+                                 			Random rollAgain2=new Random();
+														int LadderRoll2=rollAgain2.nextInt(6)+1;
+														Player2+=LadderRoll2;
+														rollDiceCount2++;
+														break;
+														
+														//snake
+                        			case 2:  Player2=Player2-rollDice2;
+                                 			break;
+
+                        			default:  System.out.println("Invalid");
+                                 			 break;
+                  			}
+
+                        if(Player2<0)
+                        {
+                              Player2=STARTPOSITION;
+                        }
+
+                        if(Player2>100)
+                        {
+                              Player2=Player2-rollDice2;
+                        }
+                        if(Player2==100)
+                        {
+                              break;
+                        }
+					}
+								if(Player1==100)
+								{
+											System.out.println("Player1 wins ");
+								}
+								else if(Player2==100)
+								{
+											System.out.println("Player2 wins");
+								}
+
+		}
 }
